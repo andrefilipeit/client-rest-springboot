910c5e6dc7bdabab50cf7d5adab21fdea50f59a5
Andre Filipe <andre.filipe.it@gmail.com>
2023-11-24 14:53:03 -0300

    Termination of route usage

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Book from './pages/Book';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={Login} />
+        <Route path='/' exact component={Login} />
+        <Route path='/book' component={Book} />
       </Switch>
     </BrowserRouter>
   );
